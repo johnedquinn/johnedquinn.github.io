e38cf557008a8cc5a2f2b8df73dfab2a40b99ed7
John Quinn <johnedquinn@gmail.com>
2024-10-31 19:03:11 -0700

    Updates layout

--- quartz.config.ts
+++ quartz.config.ts
@@ -8,7 +8,7 @@ import * as Plugin from "./quartz/plugins"
  */
 const config: QuartzConfig = {
   configuration: {
-    pageTitle: "🪴 Quartz 4.0",
+    pageTitle: "💥 JOHN QUINN",
     pageTitleSuffix: "",
     enableSPA: true,
     enablePopovers: true,
@@ -16,7 +16,7 @@ const config: QuartzConfig = {
       provider: "plausible",
     },
     locale: "en-US",
-    baseUrl: "quartz.jzhao.xyz",
+    baseUrl: "johnedq.com",
     ignorePatterns: ["private", "templates", ".obsidian"],
     defaultDateType: "created",
     theme: {
--- quartz.layout.ts
+++ quartz.layout.ts
@@ -1,5 +1,6 @@
 import { PageLayout, SharedLayout } from "./quartz/cfg"
 import * as Component from "./quartz/components"
+import { FileNode } from "./quartz/components/ExplorerNode"
 
 // components shared across all pages
 export const sharedPageComponents: SharedLayout = {
@@ -8,10 +9,46 @@ export const sharedPageComponents: SharedLayout = {
   afterBody: [],
   footer: Component.Footer({
     links: {
-      GitHub: "https://github.com/jackyzha0/quartz",
-      "Discord Community": "https://discord.gg/cRFFHYye7t",
+      GitHub: "https://github.com/johnedquinn",
+      LinkedIn: "https://www.linkedin.com/in/johnedquinn/",
+      Email: "mailto:lump-kinfolk.0m@icloud.com"
+      // "Discord Community": "https://discord.gg/cRFFHYye7t", // TODO: Add more links
     },
   }),
+}
+
+/**
+ * USER-DEFINED!
+ * @returns a function that sorts files by date and then alphabetically
+ */
+export function byDateAndAlphabetical(): (a: FileNode, b: FileNode) => number {
+  return (a, b) => {
+
+        // Sort order: folders first, then files. Sort folders and files alphabetically
+        if ((!a.file && !b.file) || (a.file && b.file)) {
+          // numeric: true: Whether numeric collation should be used, such that "1" < "2" < "10"
+          // sensitivity: "base": Only strings that differ in base letters compare as unequal. Examples: a ≠ b, a = á, a = A
+          var aDate = a.file?.dates?.created
+          var bDate = b.file?.dates?.created
+          if (aDate == undefined || bDate == undefined) {
+            return a.displayName.localeCompare(b.displayName, undefined, {
+              numeric: true,
+              sensitivity: "base",
+            })
+          }
+          if (aDate > bDate) {
+            return 1
+          } else {
+            return 1
+          }
+        }
+    
+        if (a.file && !b.file) {
+          return 1
+        } else {
+          return -1
+        }
+  }
 }
 
 // components for pages that display a single page (e.g. a single note)
@@ -27,7 +64,8 @@ export const defaultContentPageLayout: PageLayout = {
     Component.MobileOnly(Component.Spacer()),
     Component.Search(),
     Component.Darkmode(),
-    Component.DesktopOnly(Component.Explorer()),
+    Component.DesktopOnly(Component.Explorer({ folderDefaultState: "collapsed", sortFn: byDateAndAlphabetical(), folderClickBehavior: "link" })),
+    Component.DesktopOnly(Component.RecentNotes({ title: "Recent Posts", limit: 4 })),
   ],
   right: [
     Component.Graph(),
@@ -45,6 +83,7 @@ export const defaultListPageLayout: PageLayout = {
     Component.Search(),
     Component.Darkmode(),
     Component.DesktopOnly(Component.Explorer()),
+    Component.DesktopOnly(Component.RecentNotes({ title: "Recent Posts", limit: 4 })),
   ],
   right: [],
 }
