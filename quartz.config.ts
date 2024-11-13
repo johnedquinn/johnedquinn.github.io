f8749d252eabd5927394cb3e2bba64abef94b075
John Quinn <johnedquinn@gmail.com>
2024-11-12 21:16:56 -0800

    Adds favicon and website preview

--- quartz.config.ts
+++ quartz.config.ts
@@ -8,7 +8,7 @@ import * as Plugin from "./quartz/plugins"
  */
 const config: QuartzConfig = {
   configuration: {
-    pageTitle: "💥 JOHN QUINN",
+    pageTitle: "JOHN QUINN",
     pageTitleSuffix: "",
     enableSPA: true,
     enablePopovers: true,
